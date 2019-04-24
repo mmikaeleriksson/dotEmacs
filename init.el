8da3cd87e36d086576fd2e9cead8d03b962b8a77
Mikael Eriksson <m.mikael.eriksson@gmail.com>
2019-04-24 21:43:16 +0200

    Added gitignored file for custom-set-variables

--- init.el
+++ init.el
@@ -74,10 +74,10 @@
 ;;------------------------------------------------------------------------------
 ;;Auto-save and backup files saved in TEMP
 (setq backup-directory-alist
-          `((".*" . ,"~/.backups")))
-    (setq auto-save-file-name-transforms
-          `((".*" ,"~/.autosaves" t)))
-
+      `((".*" . ,"~/.backups")))
+(setq auto-save-file-name-transforms
+      `((".*" ,"~/.autosaves" t)))
+(setq custom-file "~/.emacs.d/auto-custom.el")
 
 ;;------------------------------------------------------------------------------
 ;;Fringe git-gutter settings
@@ -167,21 +167,3 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; END my stuff /miker
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "c9321e2db48a21fc656a907e97ee85d8cd86967855bf0bed3998bcf9195c758b" "f78de13274781fbb6b01afd43327a4535438ebaeec91d93ebdbba1e3fba34d3c" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "b86c3de12c012593d19916ee6c9b1ac6f0cbb1fdf6237ead94e577867f1e9dd2" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
- '(inhibit-startup-screen t)
- '(package-selected-packages
-   (quote
-    (multiple-cursors yascroll use-package smart-mode-line move-text helm git-gutter-fringe clang-format))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
